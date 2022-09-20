2d93d6d561943f28a9ff6c897f5afc8d0e579c46
Christoffer Carlsson <cc@christofferc.com>
2022-09-20 15:54:46 +0200

    Remove Autograph implementation

--- tests/autograph/generate-key-pair.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from '../../dist/test.js'
-import { generateKeyPair } from '../../dist/autograph.js'
-
-export default describe('generateKeyPair', () =>
-  it('should generate an Ed25519 identity key pair', async () => {
-    const { publicKey, privateKey } = await generateKeyPair()
-    expect(publicKey.byteLength).toBe(32)
-    expect(privateKey.byteLength).toBe(32)
-  }))
