17bf764f93de4aac198ebf2ea0436e80f9eca1de
Christoffer Carlsson <cc@christofferc.com>
2022-11-12 09:38:47 +0100

    Add tests for AES key generation in CBC mode

--- tests/crypto/generate-key.test.ts
+++ tests/crypto/generate-key.test.ts
@@ -2,12 +2,14 @@ import { generateKey } from '../../src/crypto'
 
 describe('generateKey', () => {
   const keySizes = [
+    { cipher: 'AES-128-CBC', keySize: 16 },
     { cipher: 'AES-128-GCM', keySize: 16 },
+    { cipher: 'AES-256-CBC', keySize: 32 },
     { cipher: 'AES-256-GCM', keySize: 32 }
   ]
 
   keySizes.forEach(({ cipher, keySize }) => {
-    it(`should generate a symmetric key of size ${keySize} for use with ${cipher}`, async () => {
+    it(`should generate a symmetric key of size ${keySize} bytes for use with ${cipher}`, async () => {
       const key = await generateKey(cipher)
       expect(key).toBeInstanceOf(Uint8Array)
       expect(key.byteLength).toEqual(keySize)
