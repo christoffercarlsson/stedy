6382edcbead1ab2eb749e142e353036f78a44da1
Christoffer Carlsson <cc@christofferc.com>
2023-05-09 10:20:01 +0200

    Upgrade dependencies

--- src/crypto/curve25519.ts
+++ src/crypto/curve25519.ts
@@ -1,25 +1,25 @@
-import { curve25519, getPublicKey, sign, verify, utils } from '@noble/ed25519'
+import { ed25519, x25519 } from '@noble/curves/ed25519'
 
 export const keyPair = () => {
-  const privateKey = utils.randomPrivateKey()
-  const publicKey = curve25519.scalarMultBase(privateKey)
+  const privateKey = x25519.utils.randomPrivateKey()
+  const publicKey = x25519.scalarMultBase(privateKey)
   return { publicKey, privateKey }
 }
 
 export const scalarMult = (privateKey: Uint8Array, publicKey: Uint8Array) =>
-  curve25519.scalarMult(privateKey, publicKey)
+  x25519.scalarMult(privateKey, publicKey)
 
-export const signKeyPair = async () => {
-  const privateKey = utils.randomPrivateKey()
-  const publicKey = await getPublicKey(privateKey)
+export const signKeyPair = () => {
+  const privateKey = ed25519.utils.randomPrivateKey()
+  const publicKey = ed25519.getPublicKey(privateKey)
   return { publicKey, privateKey }
 }
 
 export const signMessage = (message: Uint8Array, privateKey: Uint8Array) =>
-  sign(message, privateKey)
+  ed25519.sign(message, privateKey)
 
 export const verifyMessage = (
   signature: Uint8Array,
   message: Uint8Array,
   publicKey: Uint8Array
-) => verify(signature, message, publicKey)
+) => ed25519.verify(signature, message, publicKey)
