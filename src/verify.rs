ceca4cccc511f58ba25a20fc7c5045ff0241deff
Christoffer Carlsson <cc@christoffercarlsson.se>
2024-12-08 11:03:09 +0100

    Add tests

--- src/verify.rs
+++ src/verify.rs
@@ -1,10 +1,7 @@
 use crate::Error;
 
 pub fn verify(a: &[u8], b: &[u8]) -> Result<(), Error> {
-    if a.len() != b.len() {
-        return Err(Error::Verification);
-    }
-    let mut result = 0u8;
+    let mut result = 0;
     for (x, y) in a.iter().zip(b.iter()) {
         result |= x ^ y;
     }
