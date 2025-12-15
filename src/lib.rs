e9bf5b5ca04f0640e34c2560d1a0e95bd72a16a2
Christoffer Carlsson <cc@christoffercarlsson.com>
2025-12-15 13:42:28 +0100

    Add HKDF implementation

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod blake2s;
 mod block;
 mod chacha;
 mod chacha20poly1305;
+mod hkdf;
 mod hmac;
 mod pad;
 mod pbkdf2;
@@ -20,6 +21,6 @@ mod wipe;
 mod xor;
 
 pub use crate::{
-    blake2b::*, blake2s::*, chacha20poly1305::*, hmac::*, pad::*, pbkdf2::*, rng::*, sha256::*,
-    sha512::*, traits::Csprng, verify::*, wipe::*,
+    blake2b::*, blake2s::*, chacha20poly1305::*, hkdf::*, hmac::*, pad::*, pbkdf2::*, rng::*,
+    sha256::*, sha512::*, traits::Csprng, verify::*, wipe::*,
 };
