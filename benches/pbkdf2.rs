b4496e08d18e8c161b8fd7edf5a2e0c6a612bc6d
Christoffer Carlsson <cc@christoffercarlsson.com>
2025-12-16 09:03:20 +0100

    Add benchmarks

--- benches/pbkdf2.rs
+++ benches/pbkdf2.rs
@@ -1,19 +1,33 @@
 use criterion::Criterion;
-use stedy::{pbkdf2_hmac_sha256, pbkdf2_hmac_sha512};
+use stedy::{pbkdf2_blake2b512, pbkdf2_blake2s256, pbkdf2_hmac_sha256, pbkdf2_hmac_sha512};
 
 pub fn bench(c: &mut Criterion) {
     let password = b"password";
     let salt = b"salt";
     let iterations = 4096;
 
-    c.bench_function("pbkdf2_hmac_sha256_4096", |b| {
+    c.bench_function("pbkdf2_blake2s256", |b| {
+        b.iter(|| {
+            let mut output = [0u8; 32];
+            pbkdf2_blake2s256(password, salt, iterations, &mut output);
+        })
+    });
+
+    c.bench_function("pbkdf2_blake2b512", |b| {
+        b.iter(|| {
+            let mut output = [0u8; 64];
+            pbkdf2_blake2b512(password, salt, iterations, &mut output);
+        })
+    });
+
+    c.bench_function("pbkdf2_hmac_sha256", |b| {
         b.iter(|| {
             let mut output = [0u8; 32];
             pbkdf2_hmac_sha256(password, salt, iterations, &mut output);
         })
     });
 
-    c.bench_function("pbkdf2_hmac_sha512_4096", |b| {
+    c.bench_function("pbkdf2_hmac_sha512", |b| {
         b.iter(|| {
             let mut output = [0u8; 64];
             pbkdf2_hmac_sha512(password, salt, iterations, &mut output);
